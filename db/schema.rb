2246702d558199fc8e255ba7a49e78da64d3e8a4
root <h16861h@gmail.com>
2015-12-22 10:16:38 +0800

    email validation

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151221160253) do
+ActiveRecord::Schema.define(version: 20151222021501) do
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
@@ -20,5 +20,7 @@ ActiveRecord::Schema.define(version: 20151221160253) do
     t.datetime "updated_at",      null: false
     t.string   "password_digest"
   end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
 
 end
