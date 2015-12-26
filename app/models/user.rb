d376e0d51f93b66c40b70771aeec14fcb44e8a3b
root <h16861h@gmail.com>
2015-12-27 03:17:29 +0800

    register and a part of flash message

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,5 @@ class User < ActiveRecord::Base
 	before_save { self.email = email.downcase }
 	has_secure_password
 	validates :name, presence: true, length: { maximum: 30 } #which sets requured and length maximum
-      	validates :email, format: { with: /\A[@]+@[^@]+\z/}, uniqueness: true
+      	validates :email, format: { with: /\A[^@]+@[^@]+\z/ }, uniqueness: true #format: { with: /\A[@]+@[^@]+\z/}, 
 end
--- db/migrate/20151222021501_add_user_email_indices.rb
+++ db/migrate/20151222021501_add_user_email_indices.rb
@@ -2,4 +2,8 @@ class AddUserEmailIndices < ActiveRecord::Migration
   def change
   	add_index :users, :email, unique: true
   end
+
+  def new
+  	@user = User.new
+  end
 end
